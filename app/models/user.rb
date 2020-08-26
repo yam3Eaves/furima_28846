4e0cc4e082fe16fd36756dcde8ab1ab6b553d796
yam3Eaves <paristexaslondontokyo@gmail.com>
2020-08-26 11:49:45 +0900

    カラム名変更など

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #  with_options  presence: true do
+  #   validates :nickname, 
+  #   validates :email, uniqueness: true,
+  #   validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/}
+  #   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  #   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  #   validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/}
+  #   validates :first_name, format: { with: /\A[ァ-ン]+\z/}
+  #   validates :birthday presense: true
+  # end
 end
+
