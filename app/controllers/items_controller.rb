5f60e268339b8956ce39bad03144589c8481616f
yam3Eaves <paristexaslondontokyo@gmail.com>
2020-09-01 15:43:16 +0900

    ログイン関連の修正、追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,22 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-
+  # before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
  def index
   
  end
 
+ def new
+ end
 
 
  private
 
- def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
-  end
- end
+#  def move_to_index
+#   unless user_signed_in?
+#     redirect_to action: :index
+#   end
+#  end
 
 
 
