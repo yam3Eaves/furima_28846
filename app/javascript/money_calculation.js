355e1db964a41e0e7e74fbc7577c8a36ff252060
yam3Eaves <paristexaslondontokyo@gmail.com>
2020-09-07 12:22:35 +0900

    税と利益自動算出機能の実装

---
+++ app/javascript/money_calculation.js
@@ -0,0 +1,27 @@
+
+// DOMContentLoaded
+
+
+function calculate() {
+  
+  const get_price = document.getElementById("item-price");
+  const item_tax = document.getElementById("add-tax-price");
+  const profit_price = document.getElementById("profit");
+
+  
+    get_price.addEventListener('input', () => {
+
+      let tax = Math.round(get_price.value * 0.1);
+      let item_profit = get_price.value - tax;
+
+      item_tax.textContent = tax;
+      profit_price.textContent = item_profit;
+    
+
+    
+    });
+
+}
+
+// window.addEventListener('DOMContentLoaded', calculate)
+setInterval(calculate, 1000);
