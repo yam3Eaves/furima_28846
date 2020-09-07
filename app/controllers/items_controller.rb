c335ece21b8809d3b0602a59b000f2f6cde38949
yam3Eaves <paristexaslondontokyo@gmail.com>
2020-09-07 12:25:30 +0900

    itemsコントローラー内修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,34 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
-  before_action :authenticate_user!, except: [:index, :show]
+   before_action :authenticate_user!, except: [:index, :show]
 
  def index
   
  end
 
  def new
+  @item = Item.new
  end
+ 
+ def create
+  
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+  
+ end
+
 
 
  private
 
-#  def move_to_index
-#   unless user_signed_in?
-#     redirect_to action: :index
-#   end
-#  end
 
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_charge_bearer_id, :shipping_area_id, :days_untill_shipping_id, :price, :image).merge(user_id: current_user.id)
+  end
 
 
 
