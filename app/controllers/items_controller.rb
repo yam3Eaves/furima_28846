414b3ae8b3716e28932886278613333f56f1f640
yam3Eaves <paristexaslondontokyo@gmail.com>
2020-09-09 09:11:03 +0900

    コントローラ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
+  # ログインしていないユーザーは一覧と詳細以外にアクセスするとログイン画面に遷移
  before_action :authenticate_user!, except: [:index, :show]
+  #ログインしていても、出品者でないと編集ページに遷移しない
+ before_action :correct_user, only: [:edit, :update]
+ 
+
 
  def index
   
@@ -22,11 +27,22 @@ class ItemsController < ApplicationController
  end
 
  def show
-
    @item = Item.find(params[:id])
  end
 
+ def edit
+   @item = Item.find(params[:id])
+ end
 
+ def update
+  item = Item.find(params[:id])
+  
+   if item.update(item_params)
+    redirect_to item_path
+   else
+    render :edit
+   end
+ end
 
  private
 
@@ -35,6 +51,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_charge_bearer_id, :shipping_area_id, :days_untill_shipping_id, :price, :image).merge(user_id: current_user.id)
   end
 
-
+  def correct_user
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
 
 end
