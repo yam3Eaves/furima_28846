fbe242f1ed9de1cc9419da5f52e3f2b0caf40ea3
yam3Eaves <paristexaslondontokyo@gmail.com>
2020-09-14 19:16:51 +0900

    購入関係のコントローラー実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,54 @@
 class OrdersController < ApplicationController
+  #ログインしていないユーザーはログインページに遷移する
+  before_action :authenticate_user!, only: [:index, :create]
+  #出品者が購入ページに遷移を試みた場合、トップページに遷移
+  before_action :go_index_seller, only: [:index, :create]
+  #購入済商品の購入ページにURLから遷移しようとするとトップページに遷移する
+  before_action :go_index_soldout, only: [:index, :create]
+
+  def index  
+    @order = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit( :token, :postal_code, :prefectures_id, :city, :address, :building_name, :tel_num, :item_id).merge(user_id: current_user.id, item_id: @item.id)
+  end 
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price ,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+
+  def go_index_seller
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.id 
+      redirect_to root_path
+    end
+  end
+
+  def go_index_soldout
+    @address = Address.find(params[:item_id])
+    if @address.order_id != nil
+       redirect_to root_path
+    end
+  end
 end
